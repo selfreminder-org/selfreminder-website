e548ff50c9dec6d94629f21ac634705d22cee13e
ahlaiptek <iptekahla@gmail.com>
2026-02-04 21:05:32 +0700

    Add base layout and home page view

---
+++ resources/views/pages/home/index.blade.php
@@ -0,0 +1,3 @@
+<x-layouts::base :title="__('Self Reminder')">
+    
+</x-layouts::base>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home.index');
 })->name('home');
 
 Route::view('dashboard', 'dashboard')
